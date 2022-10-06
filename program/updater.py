10cb2d048cfa19949595b5acab855f8c9db1b7d2
rcrcue <115020933+rcrcue@users.noreply.github.com>
2022-10-06 10:20:52 +0300

    Update updater.py

--- program/updater.py
+++ program/updater.py
@@ -58,17 +58,17 @@ def updater():
 @sudo_users_only
 async def update_repo(_, message: Message):
     chat_id = message.chat.id
-    msg = await message.reply("🔄 `تتم البرمجه بواسطة سورس ايثون......`")
+    msg = await message.reply(" `المبرمج حسين`")
     update_avail = updater()
     if update_avail:
         await msg.edit("✅ update finished\n\n• bot restarted, back active again in 1 minutes.")
         system("git pull -f && pip3 install --no-cache-dir -r requirements.txt")
         execle(sys.executable, sys.executable, "main.py", environ)
         return
-    await msg.edit(f"تم الترقية بواسطه سورس [Ξ𝗜𝗧𝗛𝗢𝗡™ ايثون](https://t.me/EITHON1)", disable_web_page_preview=True)
+    await msg.edit(f"المبرمج حسين [المطور](https://t.me/rcrcu)", disable_web_page_preview=True)
 
 
-@Client.on_message(command(["ريستارت", f"restart@{BOT_USERNAME}"]) & ~filters.edited)
+@Client.on_message(command(["استارت", f"restart@{BOT_USERNAME}"]) & ~filters.edited)
 @sudo_users_only
 async def restart_bot(_, message: Message):
     msg = await message.reply("`restarting bot...`")
