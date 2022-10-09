bf4685914d241f6805e5a4b1c6d5f8cc73275913
rcrcue <115020933+rcrcue@users.noreply.github.com>
2022-10-09 03:11:29 +0300

    Update admins.py
    Signed-off-by: rcrcue <115020933+rcrcue@users.noreply.github.com>

--- program/admins.py
+++ program/admins.py
@@ -37,6 +37,16 @@ async def update_admin(client, message):
 
 @Client.on_message(command(["/skip", f"تخطي", "خطي"]) & other_filters)
 @authorized_users_only
+
+async def update_admin(client, message):
+    global admins
+    new_admins = []
+    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
+    for u in new_ads:
+        new_admins.append(u.user.id)
+    admins[message.chat.id] = new_admins
+    await message.reply_text(
+        " تم التخطي الى الاغنية الي وراها ** **  \n ** ** **! ** ")
 async def skip(c: Client, m: Message):
     user_id = m.from_user.id
     chat_id = m.chat.id
